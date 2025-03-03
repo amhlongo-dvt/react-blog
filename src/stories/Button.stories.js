213c9626185fbfd267e11078ce793b96f02a9728
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-03 13:21:00 +0200

    feat: added a post component

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,16 @@
+import { Post } from '../components/Post'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Components/Post',
+  component: Post,
+}
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary = {
+  args: {
+    title: 'Hello',
+    contents: 'Im a good boy and a billionaire',
+    author: 'Mr. West',
+  },
+}
