46696f304d2b8f0bcf8355e5453be1b5f0f93a50
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-07 14:20:01 +0200

    feat: added a filter component

---
+++ src/stories/Filter.stories copy.js
@@ -0,0 +1,10 @@
+import { PostFilter } from '../components/PostFilter'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Components/Filter',
+  component: PostFilter,
+}
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary = {}
