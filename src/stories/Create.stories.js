934f1416ce91c0e8b908c4925fee857654b5f94d
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-07 14:16:31 +0200

    feat: added a create post component

---
+++ src/stories/Create.stories.js
@@ -0,0 +1,10 @@
+import { CreatePost } from '../components/CreatePost'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Components/Create',
+  component: CreatePost,
+}
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary = {}
