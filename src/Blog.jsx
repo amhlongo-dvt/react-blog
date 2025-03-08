f26a70235c6b8e0e23607184671feeefe1ef5984
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-08 16:16:31 +0200

    feat: implementing filters and sorting

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { CreatePost } from './components/CreatePost'
 import { PostFilter } from './components/PostFilter'
@@ -6,9 +7,12 @@ import { PostSorting } from './components/PostSorting'
 import { getPosts } from './api/posts'
 
 export function Blog() {
+  const [author, setAuthor] = useState('')
+  const [sortBy, setSortBy] = useState('createdAt')
+  const [sortOrder, setSortOder] = useState('descending')
   const postsQuery = useQuery({
-    queryKey: ['posts'],
-    queryFn: () => getPosts(),
+    queryKey: ['posts', { author, sortBy, sortOrder }],
+    queryFn: () => getPosts({ author, sortBy, sortOrder }),
   })
 
   const posts = postsQuery.data ?? []
@@ -18,9 +22,19 @@ export function Blog() {
       <br />
       <hr />
       Filter by:
-      <PostFilter field='author' />
+      <PostFilter
+        field='author'
+        value={author}
+        onChange={(value) => setAuthor(value)}
+      />
       <br />
-      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <PostSorting
+        fields={['createdAt', 'updatedAt']}
+        value={sortBy}
+        onChange={(value) => setSortBy(value)}
+        orderValue={sortOrder}
+        onOrderChange={(orderValue) => setSortOder(orderValue)}
+      />
       <hr />
       <PostList posts={posts} />
     </div>
