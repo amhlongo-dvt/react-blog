69909454252c222a0f55f1bbbee1e045e9b4e621
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-08 15:16:03 +0200

    feat: Fetching blog posts

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,20 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
 import { CreatePost } from './components/CreatePost'
 import { PostFilter } from './components/PostFilter'
 import { PostList } from './components/PostList'
 import { PostSorting } from './components/PostSorting'
-
-const posts = [
-  {
-    title: 'Hello',
-    contents: 'Im a good boy and a billionaire',
-    author: 'Mr. West',
-  },
-  {
-    title: 'Hello React!',
-  },
-]
+import { getPosts } from './api/posts'
 
 export function Blog() {
+  const postsQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => getPosts(),
+  })
+
+  const posts = postsQuery.data ?? []
   return (
     <div style={{ padding: 16 }}>
       <CreatePost />
