ec3bf7ef749414af346199f22c29ef46cc42df7a
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-09 17:11:26 +0200

    feat: created the login route

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,4 +1,4 @@
-import { createUser } from '../services/users.js'
+import { createUser, loginUser } from '../services/users.js'
 
 export function userRoutes(app) {
   app.post('/api/v1/user/signup', async (req, res) => {
@@ -8,6 +8,16 @@ export function userRoutes(app) {
     } catch (err) {
       return res.status(400).json({
         error: 'failed to create the user, the username already exists.',
+      })
+    }
+  })
+  app.post('/api/v1/user/login', async (req, res) => {
+    try {
+      const token = await loginUser(req.body)
+      return res.status(200).json({ token })
+    } catch (err) {
+      return res.status(400).json({
+        error: 'login failed, did you enter the correct username/password?',
       })
     }
   })
