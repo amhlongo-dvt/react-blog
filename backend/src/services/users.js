47eb241f84512ff3e28c017c09b0e7bca499a98f
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-10 12:05:48 +0200

    feat: added a endpoint to get users info by id

--- backend/src/services/users.js
+++ backend/src/services/users.js
@@ -28,3 +28,13 @@ export async function loginUser({ username, password }) {
 
   return token
 }
+
+export async function getUserInfoById(userId) {
+  try {
+    const user = await User.findById(userId)
+    if (!user) return { username: userId }
+    return { username: user.username }
+  } catch (error) {
+    return { username: userId }
+  }
+}
