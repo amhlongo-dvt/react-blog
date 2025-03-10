10798f0c2cd14ceaad199eaea3a4346321f78003
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-10 11:49:40 +0200

    feat:  used the stored jwt  and implementing a simple logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import { Link } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
+import { jwtDecode } from 'jwt-decode'
 
 export function Header() {
-  return (
-    <div>
-      <Link to='/signup'>SignUp</Link>
-    </div>
-  )
+    const [token, setToken] = useAuth()
+    if (token) {
+        const { sub } = jwtDecode(token)
+        return (
+            <div>
+                Logged in as <b>{sub}</b>
+                <br />
+                <button onClick={() => setToken(null)}>Logout</button>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <Link to={'./login'}>Log In</Link> |{' '}
+            <Link to='/signup'>SignUp</Link>
+        </div>
+    )
 }
