a1656d772920ac5248706718627f8e1120aba5a4
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-10 13:38:50 +0200

    feat: implemented a user component to fetch and render the username

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,23 @@
 import PropTypes from 'prop-types'
+import { User } from './User'
 
 export function Post({ title, contents, author }) {
-  return (
-    <article>
-      <h3>{title}</h3>
-      <div>{contents}</div>
-      {author && (
-        <em>
-          <br />
-          Written by <strong>{author}</strong>
-        </em>
-      )}
-    </article>
-  )
+    return (
+        <article>
+            <h3>{title}</h3>
+            <div>{contents}</div>
+            {author && (
+                <em>
+                    <br />
+                    Written by <User id={author} />
+                </em>
+            )}
+        </article>
+    )
 }
 
 Post.propTypes = {
-  title: PropTypes.string.isRequired,
-  contents: PropTypes.string,
-  author: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    contents: PropTypes.string,
+    author: PropTypes.string,
 }
