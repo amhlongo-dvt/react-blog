a0085ffd2d8133916d0880eb212877920e981aa9
Andile Mhlongo <majiyaz2@gmail.com>
2025-03-16 14:00:46 +0200

    feat: added route to find post by Id

--- src/api/posts.js
+++ src/api/posts.js
@@ -19,3 +19,10 @@ export const createPost = async (token, post) => {
 
     return await res.json()
 }
+
+export const getPostsById = async (postId) => {
+    const res = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/posts/${postId}`,
+    )
+    return await res.json()
+}
