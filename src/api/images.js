98f2c84cf6d08c6dd1be9aa5ef228c9d9d9c3584
Andile Mhlongo <majiyaz2@gmail.com>
2025-04-13 16:06:36 +0200

    feat: added endpoint for images to the frontend api

---
+++ src/api/images.js
@@ -0,0 +1,21 @@
+export const createImage = async (token, image) => {
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/images?`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(image),
+    })
+
+    return await res.json()
+}
+
+export const getImageById = async (imageId) => {
+    console.log(`imageId is ${imageId}`)
+
+    const res = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/images/${imageId}`,
+    )
+    return await res.json()
+}
